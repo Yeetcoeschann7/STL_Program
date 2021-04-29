693c9abcda2a5bbae903106e5057283c39c5c479
Yeetcoeschann <pokemaniacunova@gmail.com>
2021-04-29 10:56:56 -0400

    Finished version of program. Added function to check case of calculated letter and made optimizations to the existing functions. Added goodLetter variable to make sure the calculated letter is only displayed if no exceptions were thrown.

--- Exceptions_Templates_STL/Source.cpp
+++ Exceptions_Templates_STL/Source.cpp
@@ -14,6 +14,7 @@ using namespace std;
 //function prototypes
 char character(char, int);
 void checkStart(char);
+void checkLetter(char, int);
 
 //******
 //main *
@@ -21,27 +22,44 @@ void checkStart(char);
 int main() {
 	char start, letter;
 	int offset;
+	bool goodLetter = true;
 
 	cout << "\nEnter start letter: ";
-	cin >>start;
+	cin >> start;
+
 	cout << "Enter offset: ";
 	cin >> offset;
 	validateNum(offset);
 	cout << endl;
 
-	try {
+
+	try {//Checks if start is a letter
 		checkStart(start);
+	}
+	catch (string invalidCharacterException) {
+		cout << invalidCharacterException;
+		goodLetter = false;
+	}
+
+	try {//Calculates final letter and checks if is letter
+		letter = character(start, offset);
+	}
+	catch (string invalidCaseException){
+		cout << invalidCaseException;
+		goodLetter = false;
+	}
+
+	try {//Checks if calculated character is letter
+		checkLetter(start, offset);
+
 	}
 	catch (string invalidRangeException) {
 		cout << invalidRangeException;
+		goodLetter = false;
 	}
-	
-	try {
-		letter = character(start, offset);
+
+	if (goodLetter == true) {//makes sure the calculated letter is only displayed if no exceptions were thrown.
 		cout << "\nThe calculated character is: " << letter;
-	}
-	catch (string invalidCharacterException){
-		cout << invalidCharacterException;
 	}
 
 	endFile();
@@ -63,10 +81,23 @@ void checkStart(char start) {
 char character(char start, int offset) {
 	char letter = (start + offset);
 
+	if ((isupper(start) && islower(letter)) || (isupper(letter) && islower(start))) {
+		string invalidCaseException = "ERROR: Calculated value must not change case.\n";
+		throw invalidCaseException;
+	}
+
+	return letter;
+}
+
+//*************
+//checkLetter *
+//*************
+void checkLetter(char start, int offset) {
+	char letter = (start + offset);
+
 	if (!isalpha(letter)) {
 		string invalidRangeException = "ERROR: Calculated value does not equate to an alphabetical character.\n";
 		throw invalidRangeException;
 	}
-
-	return letter;
+	
 }
