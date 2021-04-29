1dc7b3270249b2aaf6edf66e6084e368cc5d0be3
Yeetcoeschann <pokemaniacunova@gmail.com>
2021-04-29 10:13:15 -0400

    First functioning version. Still need to get program to throw an exception for upper-lower case transitions but otherwise should be similar to final version

--- Exceptions_Templates_STL/Source.cpp
+++ Exceptions_Templates_STL/Source.cpp
@@ -12,10 +12,61 @@ Class program
 using namespace std;
 
 //function prototypes
+char character(char, int);
+void checkStart(char);
 
 //******
 //main *
 //******
 int main() {
+	char start, letter;
+	int offset;
+
+	cout << "\nEnter start letter: ";
+	cin >>start;
+	cout << "Enter offset: ";
+	cin >> offset;
+	validateNum(offset);
+	cout << endl;
+
+	try {
+		checkStart(start);
+	}
+	catch (string invalidRangeException) {
+		cout << invalidRangeException;
+	}
+	
+	try {
+		letter = character(start, offset);
+		cout << "\nThe calculated character is: " << letter;
+	}
+	catch (string invalidCharacterException){
+		cout << invalidCharacterException;
+	}
+
 	endFile();
+}
+
+//************
+//checkStart *
+//************
+void checkStart(char start) {
+	if (!isalpha(start)) {
+		string invalidCharacterException = "ERROR: Start value does not equate to an alphabetical character.\n";
+		throw invalidCharacterException;
+	}
+}
+
+//***********
+//character *
+//***********
+char character(char start, int offset) {
+	char letter = (start + offset);
+
+	if (!isalpha(letter)) {
+		string invalidRangeException = "ERROR: Calculated value does not equate to an alphabetical character.\n";
+		throw invalidRangeException;
+	}
+
+	return letter;
 }
